42c84fc141798f02971f3fa23cb5bf6bea62dde3
Josh Guice <jguice@gmail.com>
2015-05-12 15:42:58 -0700

    initial version that auto-creates app token
    and finally lists all the repo's the user is watching (including private) :)

--- github-unwatcher.rb
+++ github-unwatcher.rb
@@ -1,61 +1,101 @@
 #!/usr/bin/env ruby
 
+require 'pp'
+
 require 'highline/import'
 require 'octokit'
 
-TOKEN_FILENAME = '.github-unwatcher-token'
+TOKEN_FILENAME = '.token'
+TOKEN_NAME = 'github-unwatcher'
 
-# try loading the github access token file
-begin
-  token = File.open(TOKEN_FILENAME, "r").read
-rescue Errno::ENOENT
-  # try home dir too
-  begin
-    token = File.open(File.join(Dir.home,TOKEN_FILENAME), "r").read
-  rescue Errno::ENOENT
+SCOPES = ['user','repo']
+
+
+def main
+  app_token = fetch_or_create_token
+
+  github = Octokit::Client.new(:access_token => app_token)
+
+  github.auto_paginate = true
+
+  subscriptions = github.subscriptions
+
+  subscription_names = subscriptions.collect{ |s| s[:full_name] }
+
+  subscription_names.sort!
+
+  subscription_names.each do |sub|
+    puts sub
   end
+
+  say_hi(github.user[:name])
 end
 
-if token.nil?
-puts <<-EOM
+
+def fetch_or_create_token
+  # try loading the github access token file
+  begin
+    token = File.open(TOKEN_FILENAME, "r").read
+  rescue Errno::ENOENT
+    # let errors besides 'no file there' bubble up just in case
+  end
+
+  if token.nil?
+    puts <<-EOM
 Hi, this little tool will mass-unsubscribe you from a list of github repos (one per line) piped into STDIN.
 
-Before you can run the tool you'll need to create a Personal Access Token.  To create the token, follow these steps:
+Before you can run the tool we'll need to create a Personal Access Token.  To create the token, we'll ask for your github username, password, and possibly a two-factor auth token.
 
-1) visit your Settings page at github.com (currently accessed by clicking the gear icon in the upper-right corner).
-2) click Applications on the left, then under "Personal access tokens" click the "Generate new token" button.
-3) enter a name for the token (maybe github-unwatcher?), uncheck all scopes *except* user (least necessary access is good), and click "Generate token"
-4) copy the token and put it in a file named #{TOKEN_FILENAME} in the same dir as this script (or in your home dir if you prefer)
+This will only happen the first time you run this script or if the token disappears.  The Personal Access Token will be dropped into a file called #{TOKEN_FILENAME} in this directory should you need it.
 
-All done?  Try running the script again.  :)
+The script will only be given the #{SCOPES} roles which will allow it to see and manage which repos you're subscribed to.
+
+Ready?  Here we go. :)
 
 EOM
-exit
+
+    username = ask("Github username: ")
+    password = ask("Password (trust me, it's fine): ") { |q| q.echo = "*" }
+
+    github = Octokit::Client.new(:login => username, :password => password)
+
+    stack = Faraday::RackBuilder.new do |builder|
+      #builder.response :logger # uncomment me to enable direct console logging of requests
+      builder.use Octokit::Response::RaiseError
+      builder.adapter Faraday.default_adapter
+    end
+    Octokit.middleware = stack
+
+    begin
+      github.create_authorization(:scopes => SCOPES, :note => TOKEN_NAME)
+    rescue Octokit::OneTimePasswordRequired
+      two_factor_token = ask("Two-Factor Auth Token: ")
+
+      token = github.create_authorization(
+        :scopes => SCOPES,
+        :note => TOKEN_NAME,
+        :headers => { 'X-GitHub-OTP' => two_factor_token }
+      )
+
+      token = token[:token] # get actual token from Sawyer::Resource returned by create_authorization
+    end
+
+    if token.nil?
+      puts "Sorry, I wasn't able to create an access token :("
+      exit 1
+    else
+      # store the token for later runs
+      File.open(TOKEN_FILENAME, 'w') { |f| f.write(token) }
+    end
+
+  end
+
+  token
 end
 
-puts token
-
-username = ask("Github username: ")
-password = ask("Password (trust me, it's fine): ") { |q| q.echo = "*" }
-
-github = Octokit::Client.new(:login => username, :password => password)
-
-begin
-  say_hi(github.user)
-rescue Octokit::OneTimePasswordRequired
-  two_factor_token = ask("Two-Factor Auth Token: ")
-
-  oauth_token = github.create_authorization(
-    :scopes => ['user'],
-    :note => 'github-unwatcher',
-    :headers => { 'X-GitHub-OTP' => two_factor_token }
-  )
-
-  github = Octokit::Client.new(:access_token => oauth_token)
-
-  say_hi(github.user)
-end
 
 def say_hi(user)
-  puts "Hi #{user} :)"
+  puts "Hi #{user} :) "
 end
+
+main
