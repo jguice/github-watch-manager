5fa9a228671dd5531c86c57f125a59562247ac35
Josh Guice <jguice@gmail.com>
2015-07-20 15:09:40 -0700

    feat(tests): lots of refactor/additions..need more

--- spec/github_watch_manager_spec.rb
+++ spec/github_watch_manager_spec.rb
@@ -3,9 +3,9 @@ require 'github_watch_manager'
 RSpec.describe GithubWatchManager do
   let(:gwm) { described_class.new }
 
-  describe '.get_token' do
+  describe '#get_token' do
     context 'when token is missing' do
-      it { expect(gwm.get_token).to be(nil) }
+      it { expect(gwm.get_token(nil)).to be(nil) }
     end
 
     context 'when the token file is present' do
@@ -14,29 +14,48 @@ RSpec.describe GithubWatchManager do
     end
   end
 
-  describe '.show_intro' do
-    it { expect{ gwm.show_intro }.to output(/github/).to_stdout }
+  describe '#show_intro' do
+    it { expect { gwm.show_intro }.to output(/github/).to_stdout }
   end
+
+  describe '#prompt_for_credentials' do
+    # TODO figure out how to do this in a way that assumes no knowledge of *how* STDIN is read (e.g. no
+    # STDIN.should_receive(:gets))
+    it 'should prompt for username and read an <enter> terminated string from STDIN'
+  end
+
+  describe '#create_and_save_app_token' do
+    it 'should create a github application token, prompting for a 2FA token first if required'
+  end
+
+  describe '#create_github_client' do
+    it 'should create a client object for interaction with the github API'
+  end
+
+  # TODO re-evaluate approach to github client creation...right now we make one internally in GWM and tweak the
+  # middleware to raise an exception that allows us to handle the 2FA case (which BTW is a silly bunch of hoops).  Then
+  # we create a separate one directly in gwm.  It really should just be the one client.  Maybe if we store the initial
+  # middleware we can put it back after the 2FA dance and continue to use the one client (which GWM would retain a
+  # reference to).  This would allow creation of the methods below using the GWM "internal" client instead of doing any
+  # of that in gwm. :)
+
+  # describe '#watch_repo' do
+  #   context 'when repo is valid' do
+  #     it 'sets subscribed => true and ignored => false'
+  #   end
+
+  #   context 'when repo is invalid' do
+  #     it 'returns a helpful message/exception'
+  #   end
+  # end
+
+  # describe '#unwatch_repo' do
+  #   context 'when repo is valid' do
+  #     it 'sets subscribed => false and ignored => true'
+  #   end
+
+  #   context 'when repo is invalid' do
+  #     it 'returns a helpful message/exception'
+  #   end
+  # end
 end
-
-# describe SimpleController do
-
-#   before(:each) do
-#     @controller = SimpleController.new()
-#   end
-
-#   describe '#initialize' do
-#     it 'returns a properly initialized SimpleController instance' do
-#       expect(@controller.class.to_s).to eq 'SimpleController'
-#     end
-
-#     it 'has a read/write params attribute' do
-#       @controller.params[:foo] = 'bar'
-
-#       expect(@controller.params[:foo]).to eq 'bar'
-#     end
-
-#     it 'has a read/write messages attribute' do
-#       @controller.messages[:error] = 'DANGER'
-
-#       expect(@controller.messages[:error]).to eq 'DANGER'
--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -24,36 +24,9 @@ RSpec.describe Util do
     context 'when list is not a list-like object' do
       it { expect { listify('Hi Mom!') }.to raise_exception(ArgumentError) }
     end
-  # describe '.get_token' do
-  #   context 'when token is missing' do
-  #     it { expect(manager.get_token).to be(nil) }
-  #   end
+  end
 
-  #   context 'when the token file is present' do
-  #     it { expect(manager.get_token("#{fixtures}/good.token")).to eq('6ceeaaf2f4c03097ea0be1264565ed2493d4b25c') }
-  #     it { expect(manager.get_token("#{fixtures}/bad.token")).not_to eq('6ceeaaf2f4c03097ea0be1264565ed2493d4b25c') }
-  #   end
+  describe 'parse_options' do
+    it 'should parse options correctly'
   end
 end
-
-# describe SimpleController do
-
-#   before(:each) do
-#     @controller = SimpleController.new()
-#   end
-
-#   describe '#initialize' do
-#     it 'returns a properly initialized SimpleController instance' do
-#       expect(@controller.class.to_s).to eq 'SimpleController'
-#     end
-
-#     it 'has a read/write params attribute' do
-#       @controller.params[:foo] = 'bar'
-
-#       expect(@controller.params[:foo]).to eq 'bar'
-#     end
-
-#     it 'has a read/write messages attribute' do
-#       @controller.messages[:error] = 'DANGER'
-
-#       expect(@controller.messages[:error]).to eq 'DANGER'
